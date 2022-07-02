07796e64ff73058a302f513dace1c240e9c01e82
Getabalew Tesfaye <getudesta4@gmail.com>
2022-07-02 23:47:41 +0300

    fixs

--- app.js
+++ app.js
@@ -4,51 +4,50 @@ const bodyparser = require("body-parser");
 
 const app = express();
 const nodemailer = require('nodemailer');
-app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 
-app.route("/")
-.get((req, res)=>{
+app.get(("/"),(req, res)=>{
 
     res.render("home", {title: "Home Page"})
-})
-.post((req, res)=>{
-        // const email = req.body.email;
-        // const fullName = req.body.name;
-        // const subject = req.body.subject;
-        // const message = req.body.message;
+});
+// .post((req, res)=>{
+//         // const email = req.body.email;
+//         // const fullName = req.body.name;
+//         // const subject = req.body.subject;
+//         // const message = req.body.message;
 
-        // const mObject = [email, fullName, subject, message];
+//         // const mObject = [email, fullName, subject, message];
 
-        // console.log(mObject);
+//         // console.log(mObject);
 
-        // const transporter = nodemailer.createTransport({
-        //   port: 465,
-        //   host: "smtp.gmail.com",
-        //   auth: {
-        //     user: "getushahd@gmail.com",
-        //     pass: "getabalew"
-        //   },
-        //   secure: true
-        // });
+//         // const transporter = nodemailer.createTransport({
+//         //   port: 465,
+//         //   host: "smtp.gmail.com",
+//         //   auth: {
+//         //     user: "getushahd@gmail.com",
+//         //     pass: "getabalew"
+//         //   },
+//         //   secure: true
+//         // });
 
-        // const mailData = {
-        //     from: 'getushahd@gmail.com',
-        //     to: 'getushahd@gmail.com',
-        //     subject: fullName+ " : "+ subject + " " + " from " + email,
-        //     text: message
-        // }
+//         // const mailData = {
+//         //     from: 'getushahd@gmail.com',
+//         //     to: 'getushahd@gmail.com',
+//         //     subject: fullName+ " : "+ subject + " " + " from " + email,
+//         //     text: message
+//         // }
 
-        // transporter.sendMail(mailData, (err, result)=>{
-        //     if(err){
-        //         console.log(err);
-        //     } else{
-        //         res.redirect("/");
-        //     }
-        // });
-})
+//         // transporter.sendMail(mailData, (err, result)=>{
+//         //     if(err){
+//         //         console.log(err);
+//         //     } else{
+//         //         res.redirect("/");
+//         //     }
+//         // });
+// })
 const PORT = process.env.PORT;
 
 app.listen(process.env.PORT)
